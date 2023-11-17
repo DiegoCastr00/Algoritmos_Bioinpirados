2fb0eba7bc72f6539c84dcc6864096dcceb88139
Diego <diego.castro.elvira@gamil.com>
2023-11-17 00:22:45 -0600

    PSO terminado

--- PSO/pso_explicit.m
+++ PSO/pso_explicit.m
@@ -2,37 +2,55 @@ clc;
 clear;
 
 corridas = 1;
-iteraciones = 1;
+iteraciones = 100;
+
 li = -5;
 ls = 5;
 individuos = 4;
 equis = 3;
 
-velocidades = zeros(individuos,equis)
+w= 0.1;
+c1 = 1.5;
+c2 = 1.5;
 
+velocidades = zeros(individuos,equis);
 
 for c = 1:corridas
-%    poblacion = randi([li ls],individuos,equis);
-%    disp("poblacion: ")
-%    disp(poblacion)
-    poblacion = [-5, 1, 5;
-                  2, -3, 1;
-                  1, 1, 1;
-                  -5 5 5];
-    pbest = poblacion
+   poblacion = randi([li ls],individuos,equis);
+    pbest = poblacion;
+    fx = fo(poblacion);
     for g = 1:iteraciones
-        fx = fo(poblacion);
-        pobla = [poblacion, fx]
         minValue = min(fx);
-        minIndex = find(fx == minValue);
-        gbest = pobla(minIndex, :);
+        minIndex = find(fx == minValue, 1, 'first');
+        gbest = poblacion(minIndex, :);
+
+        disp("========  ENTRADA ===========")
+        velocidades;
+        poblacion;
+        fx;
+        gbest;
+        disp("========  INICIO ===========")
         
-        disp("gbest: ")
-        disp(gbest)
         for i = 1:size(poblacion, 1)
+            rand1 = rand(1)
+            rand2 = rand(1)
+            parte1 = w * velocidades(i,:)
+            parte2 = (c1*rand1)*((pbest(i,:))-(poblacion(i,:)))
+            parte3 = (c2*rand2)*((gbest)-(poblacion(i,:)))
+            v = parte1 + parte2 + parte3
+            x = poblacion(i,:) + v
+            fxhijo = fo(x)
+            poblacion(i,:) = x
+            velocidades(i,:) = v
             
-            
-%           fo_padre = fo(fx(i,:))
+            if fx(i,:) > fxhijo
+               pbest(i,:) = x
+            end
+
+            fx(i,:) = fxhijo
+           
+            disp("~~~~~~~~~~")
+
         end
     end
 end
@@ -41,3 +59,4 @@ function fx = fo(poblacion)
     cuadrado = poblacion .^ 2;
     fx = sum(cuadrado, 2);
 end
+
