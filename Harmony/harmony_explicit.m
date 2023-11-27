7c259f0979ae75eb3f5dd32aae20cbb8920a223d
Diego <diego.castro.elvira@gamil.com>
2023-11-26 21:28:47 -0600

    harmony sin sintonizar

---
+++ Harmony/harmony_explicit.m
@@ -0,0 +1,73 @@
+clc;
+clear;
+
+corridas = 1;
+iteraciones = 100;
+
+li = -10;
+ls = 10;
+armonias = 5;
+equis = armonias;
+
+raccept = 0.9;
+rPA = 0.9;
+bW = 0.1;
+
+newX = 0;
+for c = 1:corridas
+    HM = randi([li ls], armonias, equis)
+    fx = funcionObjetivo(HM)
+
+    for g = 1:iteraciones
+        xWorst = max(fx)
+        xWorstIndex = find(fx == xWorst, 1, 'first')
+        for i = 1:size(HM, 1)
+            rand1 = rand(1)
+            if rand1 < raccept 
+                index = randi(numel(HM(i,:)))
+                rand2 = rand(1)
+                if rand2 < rPA
+                    rand3 = -1 + 2 * rand
+                    newX(i,:) = HM(i,index) + bW * rand3
+                else
+                    newX(i,:) = HM(i,index)
+                end
+            else 
+                newX(i,:) =  randi([li ls])
+            end 
+
+        end
+        newX = transpose(newX)
+
+        for j = 1:numel(newX)
+            while newX(j) > ls || newX(j) < li
+                if newX(j) > ls 
+                    newX(j) = (2 * ls) - newX(j)
+                elseif newX(j) < li
+                    newX(j) = (2 * li) - newX(j)
+                else
+                    newX(j) = newX(j)
+                end
+            end
+        end
+
+        newXfo = funcionObjetivo(newX)
+        if newXfo < xWorst
+            HM(xWorstIndex, :) = newX;
+            fx(xWorstIndex, :) = newXfo;
+        else
+            HM = HM;
+            fx = fx;
+        end
+        newX = [];
+    end
+    HM
+    fx
+end
+
+function fx = funcionObjetivo(poblacion)
+    cuadrado = poblacion .^ 2;
+    fx = sum(cuadrado, 2);
+end
+
+
---
+++ diferencial/examen_DiegoCastro.m
@@ -0,0 +1,74 @@
+clc;
+clear;
+
+corridas = 30;
+iteraciones = 1000;
+
+li = -10;
+ls = 10;
+individuos = 10;
+equis = 2;
+
+w = 0.6;
+c1 = 2;
+c2 = 2.5;
+
+mejores =[];
+for c = 1:corridas
+    velocidades = zeros(individuos, equis);
+    poblacion = randi([li ls], individuos, equis);
+    pbest = poblacion;
+    fx = objetivo(poblacion);
+
+    for g = 1:iteraciones
+        minValue = min(fx);
+        minIndex = find(fx == minValue, 1, 'first');
+        gbest = poblacion(minIndex, :);
+
+        for i = 1:size(poblacion, 1)
+            rand1 = rand(1);
+            rand2 = rand(1);
+            parte1 = w * velocidades(i, :);
+            parte2 = (c1 * rand1) * (pbest(i, :) - poblacion(i, :));
+            parte3 = (c2 * rand2) * (gbest - poblacion(i, :));
+            v = parte1 + parte2 + parte3;
+            x = poblacion(i, :) + v;
+            fxhijo = objetivo(x); 
+            
+            poblacion(i, :) = x;
+            velocidades(i, :) = v;
+
+            if fx(i, :) > fxhijo
+               pbest(i, :) = x;
+            end
+
+            fx(i, :) = fxhijo;
+
+        end
+    end
+    final = [poblacion, fx;];
+    mejor = final(1, :);
+    mejores = [mejores; mejor];
+end
+disp("Resultado de 30 corridas: ")
+disp(mejores)
+disp("Costo computacional: ")
+costo = individuos * iteraciones;
+disp(costo)
+resultados_fo = final(:, end);
+
+desviacion_estandar = std(resultados_fo)
+[mejor_resultado, mejor_corrida] = min(resultados_fo);
+[peor_resultado, peor_corrida] = max(resultados_fo);
+corrida_promedio = mean(resultados_fo)
+
+fila_mejor = final(mejor_corrida, :)
+fila_peor = final(peor_corrida, :)
+
+
+function fx = objetivo(poblacion)
+    x1=poblacion(:, 1);
+    x2=poblacion(:, 2);
+    fx = (2*x1 + 3*x2.^2 - 22).^2 + (2*x1 + x2 - 8).^2;
+end
+
